2c52eedbc10352a436ec7ddc61d7369efaaa2c23
roylyh <roy.louyihui@gmail.com>
2023-03-23 20:55:43 -0700

    InterviewerListItem component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+import classNames from "classnames";
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+
+  const InterviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
+  return (
+    <li
+      className={InterviewerClass}
+      onClick={() => {
+        props.setInterviewer(props.id);
+      }}
+    >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.name}
+    </li>
+  );
+}
