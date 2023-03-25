958a0e6134b27d2851aa91efaeea40e1b9abaa5f
roylyh <roy.louyihui@gmail.com>
2023-03-24 18:41:33 -0700

    empty header show components and stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 
+import Header from "./Header";
 import "components/Appointment/styles.scss";
+
 
 export default function Appointment() {
   return <article className="appointment"></article>;
