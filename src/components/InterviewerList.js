301696d582a9a49838555921e9f872c7a238b400
roylyh <roy.louyihui@gmail.com>
2023-03-28 20:45:05 -0700

    checking with prop types

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
 
@@ -8,10 +9,23 @@ export default function InterviewerList(props) {
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
         {props.interviewers.map((interviewer) => {
-          return <InterviewerListItem key={interviewer.id} name={interviewer.name} avatar={interviewer.avatar} selected={interviewer.id === props.value} setInterviewer={()=>{props.onChange(interviewer.id)}} />
-        }
-        )}
+          return (
+            <InterviewerListItem
+              key={interviewer.id}
+              name={interviewer.name}
+              avatar={interviewer.avatar}
+              selected={interviewer.id === props.value}
+              setInterviewer={() => {
+                props.onChange(interviewer.id);
+              }}
+            />
+          );
+        })}
       </ul>
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
